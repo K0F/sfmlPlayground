024a39855000226f92fce2e5ac997b2bee29fc13
k0f <krystof.pesek@gmail.com>
2016-01-19 04:27:30 +0100

    AutoCommit @ Tue Jan 19 04:27:30 CET 2016

--- Hello.cpp
+++ Hello.cpp
@@ -22,161 +22,162 @@ using namespace std;
 ////////////////////////////////////////////////////////////
 int main()
 {
-    // Request a 24-bits depth buffer when creating the window
-    sf::ContextSettings contextSettings;
-    contextSettings.depthBits = 24;
+  // Request a 24-bits depth buffer when creating the window
+  sf::ContextSettings contextSettings;
+  contextSettings.depthBits = 24;
 
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(640, 480), "Hello World OPENGL", sf::Style::Default, contextSettings);
-    window.setVerticalSyncEnabled(true);
-    
-    // Make it the active window for OpenGL calls
-    window.setActive();
+  // Create the main window
+  sf::RenderWindow window(sf::VideoMode(640, 480), "Hello World OPENGL", sf::Style::Default, contextSettings);
+  window.setVerticalSyncEnabled(true);
+  window.setFramerateLimit(60);
 
-
-    sf::Font font;
-    if(!font.loadFromFile("resources/SempliceRegular.ttf"))
-      return EXIT_FAILURE;
+  // Make it the active window for OpenGL calls
+  window.setActive();
 
 
-    sf::Text text("Hello world!",font);
-    text.setColor(sf::Color(255,255,255,170));
-    text.setPosition(20.f,20.f);
-    text.setCharacterSize(8);
+  sf::Font font;
+  if(!font.loadFromFile("resources/SempliceRegular.ttf"))
+    return EXIT_FAILURE;
 
-    // Set the color and depth clear values
-    glClearDepth(1.f);
-    glClearColor(0.f, 0.f, 0.f, 1.f);
 
-    // Enable Z-buffer read and write
-    glEnable(GL_DEPTH_TEST);
-    glDepthMask(GL_TRUE);
+  sf::Text text("Hello world!",font);
+  text.setColor(sf::Color(255,255,255,170));
+  text.setPosition(20.f,20.f);
+  text.setCharacterSize(8);
 
-    // Disable lighting and texturing
-    glDisable(GL_LIGHTING);
-    glDisable(GL_TEXTURE_2D);
+  // Set the color and depth clear values
+  glClearDepth(1.f);
+  glClearColor(0.f, 0.f, 0.f, 1.f);
 
-    // Configure the viewport (the same size as the window)
-    glViewport(0, 0, window.getSize().x, window.getSize().y);
+  // Enable Z-buffer read and write
+  glEnable(GL_DEPTH_TEST);
+  glDepthMask(GL_TRUE);
 
-    // Setup a perspective projection
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    GLfloat ratio = static_cast<float>(window.getSize().x) / window.getSize().y;
-    glFrustum(-ratio, ratio, -1.f, 1.f, 1.f, 500.f);
+  // Disable lighting and texturing
+  glDisable(GL_LIGHTING);
+  glDisable(GL_TEXTURE_2D);
 
-    // Define a 3D cube (6 faces made of 2 triangles composed by 3 vertices)
-    GLfloat cube[] =
+  // Configure the viewport (the same size as the window)
+  glViewport(0, 0, window.getSize().x, window.getSize().y);
+
+  // Setup a perspective projection
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  GLfloat ratio = static_cast<float>(window.getSize().x) / window.getSize().y;
+  glFrustum(-ratio, ratio, -1.f, 1.f, 1.f, 500.f);
+
+  // Define a 3D cube (6 faces made of 2 triangles composed by 3 vertices)
+  GLfloat cube[] =
+  {
+    // positions    // colors (r, g, b, a)
+    -50, -50, -50,  0, 0, 1, 1,
+    -50,  50, -50,  0, 0, 1, 1,
+    -50, -50,  50,  0, 0, 1, 1,
+    -50, -50,  50,  0, 0, 1, 1,
+    -50,  50, -50,  0, 0, 1, 1,
+    -50,  50,  50,  0, 0, 1, 1,
+
+    50, -50, -50,  0, 1, 0, 1,
+    50,  50, -50,  0, 1, 0, 1,
+    50, -50,  50,  0, 1, 0, 1,
+    50, -50,  50,  0, 1, 0, 1,
+    50,  50, -50,  0, 1, 0, 1,
+    50,  50,  50,  0, 1, 0, 1,
+
+    -50, -50, -50,  1, 0, 0, 1,
+    50, -50, -50,  1, 0, 0, 1,
+    -50, -50,  50,  1, 0, 0, 1,
+    -50, -50,  50,  1, 0, 0, 1,
+    50, -50, -50,  1, 0, 0, 1,
+    50, -50,  50,  1, 0, 0, 1,
+
+    -50,  50, -50,  0, 1, 1, 1,
+    50,  50, -50,  0, 1, 1, 1,
+    -50,  50,  50,  0, 1, 1, 1,
+    -50,  50,  50,  0, 1, 1, 1,
+    50,  50, -50,  0, 1, 1, 1,
+    50,  50,  50,  0, 1, 1, 1,
+
+    -50, -50, -50,  1, 0, 1, 1,
+    50, -50, -50,  1, 0, 1, 1,
+    -50,  50, -50,  1, 0, 1, 1,
+    -50,  50, -50,  1, 0, 1, 1,
+    50, -50, -50,  1, 0, 1, 1,
+    50,  50, -50,  1, 0, 1, 1,
+
+    -50, -50,  50,  1, 1, 0, 1,
+    50, -50,  50,  1, 1, 0, 1,
+    -50,  50,  50,  1, 1, 0, 1,
+    -50,  50,  50,  1, 1, 0, 1,
+    50, -50,  50,  1, 1, 0, 1,
+    50,  50,  50,  1, 1, 0, 1,
+  };
+
+  // Enable position and color vertex components
+  glEnableClientState(GL_VERTEX_ARRAY);
+  glEnableClientState(GL_COLOR_ARRAY);
+  glVertexPointer(3, GL_FLOAT, 7 * sizeof(GLfloat), cube);
+  glColorPointer(4, GL_FLOAT, 7 * sizeof(GLfloat), cube + 3);
+
+  // Disable normal and texture coordinates vertex components
+  glDisableClientState(GL_NORMAL_ARRAY);
+  glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+
+  // Create a clock for measuring the time elapsed
+  sf::Clock clock;
+
+  ///////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  // Start the game loop
+  while (window.isOpen())
+  {
+    // Process events
+    sf::Event event;
+    while (window.pollEvent(event))
     {
-        // positions    // colors (r, g, b, a)
-        -50, -50, -50,  0, 0, 1, 1,
-        -50,  50, -50,  0, 0, 1, 1,
-        -50, -50,  50,  0, 0, 1, 1,
-        -50, -50,  50,  0, 0, 1, 1,
-        -50,  50, -50,  0, 0, 1, 1,
-        -50,  50,  50,  0, 0, 1, 1,
+      // Close window: exit
+      if (event.type == sf::Event::Closed)
+        window.close();
 
-         50, -50, -50,  0, 1, 0, 1,
-         50,  50, -50,  0, 1, 0, 1,
-         50, -50,  50,  0, 1, 0, 1,
-         50, -50,  50,  0, 1, 0, 1,
-         50,  50, -50,  0, 1, 0, 1,
-         50,  50,  50,  0, 1, 0, 1,
+      // Escape key: exit
+      if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
+        window.close();
 
-        -50, -50, -50,  1, 0, 0, 1,
-         50, -50, -50,  1, 0, 0, 1,
-        -50, -50,  50,  1, 0, 0, 1,
-        -50, -50,  50,  1, 0, 0, 1,
-         50, -50, -50,  1, 0, 0, 1,
-         50, -50,  50,  1, 0, 0, 1,
-
-        -50,  50, -50,  0, 1, 1, 1,
-         50,  50, -50,  0, 1, 1, 1,
-        -50,  50,  50,  0, 1, 1, 1,
-        -50,  50,  50,  0, 1, 1, 1,
-         50,  50, -50,  0, 1, 1, 1,
-         50,  50,  50,  0, 1, 1, 1,
-
-        -50, -50, -50,  1, 0, 1, 1,
-         50, -50, -50,  1, 0, 1, 1,
-        -50,  50, -50,  1, 0, 1, 1,
-        -50,  50, -50,  1, 0, 1, 1,
-         50, -50, -50,  1, 0, 1, 1,
-         50,  50, -50,  1, 0, 1, 1,
-
-        -50, -50,  50,  1, 1, 0, 1,
-         50, -50,  50,  1, 1, 0, 1,
-        -50,  50,  50,  1, 1, 0, 1,
-        -50,  50,  50,  1, 1, 0, 1,
-         50, -50,  50,  1, 1, 0, 1,
-         50,  50,  50,  1, 1, 0, 1,
-    };
-
-    // Enable position and color vertex components
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
-    glVertexPointer(3, GL_FLOAT, 7 * sizeof(GLfloat), cube);
-    glColorPointer(4, GL_FLOAT, 7 * sizeof(GLfloat), cube + 3);
-
-    // Disable normal and texture coordinates vertex components
-    glDisableClientState(GL_NORMAL_ARRAY);
-    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-
-    // Create a clock for measuring the time elapsed
-    sf::Clock clock;
-
-    ///////////////////////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////////////////////
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed)
-                window.close();
-
-            // Escape key: exit
-            if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
-                window.close();
-
-            // Resize event: adjust the viewport
-            if (event.type == sf::Event::Resized)
-                glViewport(0, 0, event.size.width, event.size.height);
-        }
-
-        // Clear the color and depth buffers
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Apply some transformations to rotate the cube
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glTranslatef(0.f, 0.f, -200.f);
-        glRotatef(clock.getElapsedTime().asSeconds() * 500, 1.f, 0.f, 0.f);
-        glRotatef(clock.getElapsedTime().asSeconds() * 30, 0.f, 1.f, 0.f);
-        glRotatef(clock.getElapsedTime().asSeconds() * 90, 0.f, 0.f, 1.f);
-
-        // Draw the cube
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
-        
-        std::ostringstream ss;
-        ss << (frameCount / clock.getElapsedTime().asSeconds()) << " FPS";
-        text.setString(ss.str());
-
-        window.pushGLStates();
-        window.draw(text);
-        window.popGLStates();
-
-        // Finally, display the rendered frame on screen
-        window.display();
-
-        frameCount++;
+      // Resize event: adjust the viewport
+      if (event.type == sf::Event::Resized)
+        glViewport(0, 0, event.size.width, event.size.height);
     }
 
-    return EXIT_SUCCESS;
+    // Clear the color and depth buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Apply some transformations to rotate the cube
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.f, 0.f, -200.f);
+    glRotatef(clock.getElapsedTime().asSeconds() * 500, 1.f, 0.f, 0.f);
+    glRotatef(clock.getElapsedTime().asSeconds() * 30, 0.f, 1.f, 0.f);
+    glRotatef(clock.getElapsedTime().asSeconds() * 90, 0.f, 0.f, 1.f);
+
+    // Draw the cube
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+    std::ostringstream ss;
+    ss << (frameCount / clock.getElapsedTime().asSeconds()) << " FPS";
+    text.setString(ss.str());
+
+    window.pushGLStates();
+    window.draw(text);
+    window.popGLStates();
+
+    // Finally, display the rendered frame on screen
+    window.display();
+
+    frameCount++;
+  }
+
+  return EXIT_SUCCESS;
 }
 
 
